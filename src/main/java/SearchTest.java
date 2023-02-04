19d9270096bc0f7430ecc14dcfcaa0bc46c2852b
alex@trustedpartners.com <alex@trustedpartners.com>
2023-02-04 11:09:47 +0200

    Add static assertions and AssertJ

--- src/main/java/SearchTest.java
+++ src/main/java/SearchTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,10 +7,12 @@ import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
 
 public class SearchTest {
         @Test
-        public void  openGoogleComInChromeTest(){
+        public void  openGoogleComInChromeTest() throws InterruptedException {
                 File file = new File("/Users/alex/seleniumFramework/src/resources/chromedriver");
                 System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
                 WebDriver driver = new ChromeDriver();
@@ -17,7 +20,14 @@ public class SearchTest {
                 System.out.println(driver.getTitle());
                 WebElement searchField = driver.findElement(By.name("q"));
 
-
+                searchField.click();
+                searchField.sendKeys("selenium java");
+                searchField.sendKeys(Keys.RETURN);
+                Thread.sleep(3000);
+                WebElement resultRow = driver.findElement(By.xpath("//div[@class='yuRUbf']//h3"));
+                assertTrue(resultRow.isDisplayed(),"Element has not been displayed");
+                assertEquals(resultRow.getText(), "The Selenium Browser Automation Project");
+                assertEquals(resultRow.getAttribute("class"), "LC20lb MBeuO DKV0Md");
                 driver.quit();
         }
 
